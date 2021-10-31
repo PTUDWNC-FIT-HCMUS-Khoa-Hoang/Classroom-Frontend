1506947e08548999f7946751f7166d923bdcfcb3
Hoang <hoangvnl57@gmail.com>
2021-10-31 23:09:44 +0700

    Add title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import ClassList from "./Components/Classroom/ClassList";
 import Header from "./Components/Header/Header";
+import { useEffect } from "react";
 const App = () => {
+  useEffect(() => {
+    document.title = "Classroom";
+  }, []);
   return (
     <div className="App">
       <Header />
